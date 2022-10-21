4f6f8f4561bf7865045d5c42e54476f71ab8c557
Maeziii <jf09@uw.edu>
2022-10-20 23:18:57 -0700

    moved logo & added dynamic input

--- src/components/ImageLinkForm/ImageLinkForm.js
+++ src/components/ImageLinkForm/ImageLinkForm.js
@@ -1,6 +1,6 @@
 import './ImageLinkForm.css';
 
-const ImageLinkForm = () => {
+const ImageLinkForm = ({ onInputChange, onSubmit }) => {
   return (
     <div>
       <p className='f3 white'>
@@ -8,8 +8,8 @@ const ImageLinkForm = () => {
 			</p>
 			<div className='center'>
 				<div className='form center pa4 br3'>
-					<input className='f4 pa2 w-70 center' type='text'></input>
-					<button className='w-30 grow f4 link ph3 pv2 dib black bg-green'>Detect</button>
+					<input className='f4 pa2 w-70 center' type='tex' onChange={onInputChange}></input>
+					<button className='w-30 grow f4 link ph3 pv2 dib black bg-green' onClick={onSubmit}>Detect</button>
 				</div>
 			</div>
     </div>
--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -3,7 +3,7 @@ import brain from './brain.png';
 
 const Logo = () => {
   return (
-    <div className='ma4 mt0'>
+    <div className='ma4' style={{position: 'absolute', top: '0', left: '0'}}>
       <Tilt className='br3 pa3' style={{height: '150px', width: '150px', background: '#19a974', margin: '0'}}>      
         <img style={{paddingTop: '5px'}} src={brain} alt='brainlogo'></img>
       </Tilt>
