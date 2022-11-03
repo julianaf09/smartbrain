315ba7b5c106d1c46e05799523a517e6541d178d
Maeziii <jf09@uw.edu>
2022-11-03 12:18:57 -0700

    updated fetch

--- src/components/Signin/Signin.js
+++ src/components/Signin/Signin.js
@@ -18,7 +18,7 @@ class Signin extends React.Component {
 	}
 
 	onSubmitSignIn = () => {
-		fetch('http://localhost:3000/signin', {
+		fetch('https://frozen-island-74142.herokuapp.com/signin', {
 			method: 'post',
 			headers: {'Content-Type': 'application/json'},
 			body: JSON.stringify({
